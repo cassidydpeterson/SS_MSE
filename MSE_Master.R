999511804beedf8262b867b6883202a57b415108
cassidydpeterson <cassidydpeterson@gmail.com>
2020-07-07 12:28:42 -0400

    Speed up simulation; run EM with -nohess and start next EM with control.ss_new from last run

--- MSE_Master.R
+++ MSE_Master.R
@@ -162,6 +162,8 @@ MSE_func = function(MCMCdir, OMdir, EMdir, StoreResults, FRQ=5,
       # RUN EM
       #-------------------------------------------------------------------------------------------------------------
       RunEM(EMdir)
+      # Copy control.ss_new to SB.ctl for next run -- to help starting values for next run. 
+      file.copy(from=paste0(EMdir,"\\control.ss_new"),to=paste0(EMdir,"\\SB.ctl"))
       
       
       #-------------------------------------------------------------------------------------------------------------
